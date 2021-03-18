482a947692190388dbbbc410646b6b9d1c0b6375
Alfred <alfred.b.wong@gmail.com>
2021-03-18 13:47:45 -0400

    Add some attrs and make button change color

--- app/src/main/java/com/udacity/LoadingButton.kt
+++ app/src/main/java/com/udacity/LoadingButton.kt
@@ -7,17 +7,20 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.util.Log
+import android.view.MotionEvent
 import android.view.View
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getColor
+import androidx.core.content.withStyledAttributes
 import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
-    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
     private var widthSize = 0
     private var heightSize = 0
 
     private val valueAnimator = ValueAnimator()
-
 
     private val paint = Paint().apply {
         // Smooth out edges of what is drawn without affecting shape.
@@ -26,36 +29,60 @@ class LoadingButton @JvmOverloads constructor(
         textSize = resources.getDimension(R.dimen.textSize)
         textAlign = Paint.Align.CENTER
     }
+    //Paint for the bg of the loading button
+    private val bgPaint = Paint().apply {
+        style = Paint.Style.FILL
+        color = Color.GREEN
+    }
     private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
-
+        when (new){
+            ButtonState.Loading -> {
+                bgPaint.color = Color.YELLOW
+            }
+            ButtonState.Clicked -> {
+                bgPaint.color = Color.RED
+            }
+            ButtonState.Completed -> {
+                bgPaint.color = Color.GREEN
+            }
+        }
     }
 
 
     init {
-
+        isClickable = true
     }
 
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        canvas.drawColor(Color.argb(255,60,204,153))
+
+        Log.i("LoadingButton", bgPaint.color.toString())
+        canvas.drawColor(bgPaint.color)
+
         val textPositionX = canvas.width /2
         val textPositionY = canvas.height /2 - (paint.descent() + paint.ascent())/2
 
-        canvas.drawText("HELLO", textPositionX.toFloat(), textPositionY ,paint)
+        canvas.drawText("HELLO", textPositionX.toFloat(), textPositionY, paint)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         val minw: Int = paddingLeft + paddingRight + suggestedMinimumWidth
         val w: Int = resolveSizeAndState(minw, widthMeasureSpec, 1)
         val h: Int = resolveSizeAndState(
-            MeasureSpec.getSize(w),
-            heightMeasureSpec,
-            0
+                MeasureSpec.getSize(w),
+                heightMeasureSpec,
+                0
         )
         widthSize = w
         heightSize = h
         setMeasuredDimension(w, h)
     }
 
+    override fun performClick(): Boolean {
+        super.performClick()
+        buttonState = buttonState.next()
+        invalidate()
+        return true
+    }
 }
