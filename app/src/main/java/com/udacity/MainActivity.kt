77203426b3e4c06750711a5a6cc61e282c8a0097
Alfred <alfred.b.wong@gmail.com>
2021-03-19 09:51:56 -0400

    configure radio buttons to download from different sites

--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -9,6 +9,7 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.widget.RadioGroup
 import android.widget.Toast
@@ -37,12 +38,16 @@ class MainActivity : AppCompatActivity() {
 
         custom_button.setOnClickListener {
             val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
-
             //Check for radiobutton selected otherwise show toast
-            if (radioGroup.checkedRadioButtonId == -1){
+            val selectionId = radioGroup.checkedRadioButtonId
+            if (selectionId == -1) {
                 Toast.makeText(applicationContext, noDownloadSelectedText, Toast.LENGTH_SHORT).show()
             } else {
-                //download()
+                when (selectionId) {
+                    R.id.radioButtonGlide -> download(GLIDE_URL)
+                    R.id.radioButtonLoadApp -> download(UDACITY_URL)
+                    R.id.radioButtonRetrofit -> download(RETROFIT_URL)
+                }
             }
         }
     }
@@ -50,27 +55,46 @@ class MainActivity : AppCompatActivity() {
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+            Log.i("MainActivity", "Receiver: " + id.toString())
+            val action = intent?.action
+            Log.i("MainActivity", "Action intent: $action")
+
         }
     }
 
-    private fun download() {
+    private fun download(url: String) {
         val request =
-            DownloadManager.Request(Uri.parse(URL))
-                .setTitle(getString(R.string.app_name))
-                .setDescription(getString(R.string.app_description))
-                .setRequiresCharging(false)
-                .setAllowedOverMetered(true)
-                .setAllowedOverRoaming(true)
+                DownloadManager.Request(Uri.parse(url))
+                        .setTitle(when (url) {
+                            UDACITY_URL -> "Load-App-Starter"
+                            GLIDE_URL -> "Glide-Master"
+                            RETROFIT_URL -> "Retrofit-Master"
+                            else -> "Unknown-File"
+                        })
+                        .setDescription(getString(R.string.app_description))
+                        .setRequiresCharging(false)
+                        .setAllowedOverMetered(true)
+                        .setAllowedOverRoaming(true)
+                        .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "starter.zip")
 
         val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
         downloadID =
-            downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+                downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+
     }
 
     companion object {
-        private const val URL =
-            "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+        private const val UDACITY_URL =
+                "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+        private const val GLIDE_URL =
+                "https://github.com/bumptech/glide/archive/master.zip"
+        private const val RETROFIT_URL =
+                "https://github.com/square/retrofit/archive/master.zip"
         private const val CHANNEL_ID = "channelId"
+
+        private const val DEFAULT_UDACITY_FILE_NAME = "load-app"
+        private const val DEAFULT_GLIDE_FILE_NAME = "glide"
+        private const val DEFAULT_RETROFIT_FILE_NAME = "retrofit"
     }
 
 }
